62ff49b2ac7105dba71e4d7d1d4880f6bf05c013
Lalit sharma <geekymonkcp742k@gmail.com>
2023-06-08 19:17:08 +0530

    Header : ✅ Implemented search and create room button in header

--- frontend/src/pages/Rooms/Rooms.jsx
+++ frontend/src/pages/Rooms/Rooms.jsx
@@ -1,7 +1,29 @@
 import React from "react";
-
+import styles from "./Rooms.module.css";
+import { MdSpatialAudioOff } from "react-icons/md";
+import { BiSearchAlt2 } from "react-icons/bi";
 const Rooms = () => {
-  return <></>;
+  return (
+    <>
+      <div className="container">
+        <div className={styles.roomsHeader}>
+          <div className={styles.left}>
+            <span className={styles.heading}>All voice rooms</span>
+            <div className={styles.searchBox}>
+              <BiSearchAlt2 fontSize="24px" />
+              <input type="text" className={styles.searchInput} />
+            </div>
+          </div>
+          <div className={styles.right}>
+            <button className={styles.startRoomButton}>
+              <MdSpatialAudioOff color="#fff" fontSize="22px" />
+              <span>Start a room</span>
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Rooms;
