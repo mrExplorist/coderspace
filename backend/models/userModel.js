95b2fae40c8fb71d82f68fed602d70d946151bdd
Lalit sharma <geekymonkcp742k@gmail.com>
2023-06-12 20:41:47 +0530

    Implementation of getter function for avatar URL construction

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -6,11 +6,18 @@ var UserSchema = new Schema(
     phone: { type: String, required: true },
 
     name: { type: String, required: false },
-    avatar: { type: String, required: false },
+    avatar: {
+      type: String,
+      required: false,
+      get: (avatar) => {
+        return `${process.env.BASE_URL}${avatar}`;
+      },
+      // In JavaScript, a getter function is a special type of function that is used to retrieve the value of an object's property. It allows you to define custom logic for accessing a property, instead of directly accessing the property value.
+    },
 
     isActivated: { type: Boolean, required: false, default: false },
   },
-  { timestamps: true }
+  { timestamps: true, toJSON: { getters: true } }
 );
 
 module.exports = mongoose.model("User", UserSchema, "users");
