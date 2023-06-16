f8b93d5d9facecf544b5d4172bfd6d9274c91625
Lalit sharma <geekymonkcp742k@gmail.com>
2023-06-17 00:06:45 +0530

    RoomUI for joinees

--- frontend/src/pages/Room/Room.jsx
+++ frontend/src/pages/Room/Room.jsx
@@ -1,8 +1,12 @@
 import React, { useState } from "react";
 import { useWebRTC } from "../../hooks/useWebRTC";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+
 import { useSelector } from "react-redux";
 
+import { BsArrowLeft } from "react-icons/bs";
+import { IoMdMicOff } from "react-icons/io";
+import { FaHandPeace, FaHandSparkles } from "react-icons/fa";
 import styles from "./Room.module.css";
 const Room = () => {
   const { id: roomId } = useParams();
@@ -10,22 +14,59 @@ const Room = () => {
 
   const { clients, provideRef } = useWebRTC(roomId, user);
 
+  const navigate = useNavigate();
+
+  const handleManualLeave = () => {
+    navigate("/rooms");
+  };
   return (
     <div>
-      <h1>All Connected clients</h1>
-      {clients.map((client) => {
-        return (
-          <div className={styles.userHead} key={client.id}>
-            <audio
-              ref={(instance) => provideRef(instance, client.id)} //ref attribute is used to create a reference to the rendered <audio> element. This reference can then be used to interact with or manipulate the <audio> element programmatically.
-              // controls
-              autoPlay
-            ></audio>
-            <img className={styles.avatar} src={client.avatar} alt="avatar" />
-            <h4>{client.name}</h4>
+      <div className="container">
+        <button className={styles.goBack} onClick={handleManualLeave}>
+          <BsArrowLeft fontSize={18} />
+
+          <span> All voice rooms</span>
+        </button>
+      </div>
+      <div className={styles.clientsWrap}>
+        <div className={styles.header}>
+          <h2 className={styles.topic}>Boost Investment Portfolio</h2>
+          <div className={styles.actions}>
+            <button>
+              <FaHandSparkles color="yellow" fontSize={24} />
+            </button>
+            <button onClick={handleManualLeave}>
+              <FaHandPeace color="#F2BC1D" fontSize={22} />
+              <span>Leave quietly</span>
+            </button>
           </div>
-        );
-      })}
+        </div>
+        <div className={styles.clientsList}>
+          {clients.map((client) => {
+            return (
+              <div className={styles.client}>
+                <div className={styles.userHead} key={client.id}>
+                  <audio
+                    ref={(instance) => provideRef(instance, client.id)} //ref attribute is used to create a reference to the rendered <audio> element. This reference can then be used to interact with or manipulate the <audio> element programmatically.
+                    // controls
+                    // autoPlay
+                  ></audio>
+                  <img
+                    className={styles.avatar}
+                    src={client.avatar}
+                    alt="avatar"
+                  />
+                  <button className={styles.micbtn}>
+                    {/* <BsFillMicFill color="#fff" fontSize={18} /> */}
+                    <IoMdMicOff color="#fff" fontSize={22} />
+                  </button>
+                  <h4>{client.name}</h4>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 };
